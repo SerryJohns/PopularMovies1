28880430bfe4d30b44f18fb9b12cddacc0d04baa
serryjohns <serembaj@gmail.com>
2020-03-29 22:39:00 +0300

    Populate interface screens

---
+++ app/src/main/java/com/example/popularmovies/data/Repository.java
@@ -0,0 +1,41 @@
+package com.example.popularmovies.data;
+
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
+
+public class Repository {
+    public static List<Movie> getDummyMovies() {
+        return Arrays.asList(
+                new Movie("Bloodshot 1",
+                        LocalDate.of(2020, 02, 20),
+                        "/8WUVHemHFH2ZIP6NWkwlHWsyrEL.jpg",
+                        "After he and his wife are murdered, marine Ray Garrison is resurrected by a team of scientists. Enhanced with nanotechnology, he becomes a superhuman, biotech killing machine—'Bloodshot'. As Ray first trains with fellow super-soldiers, he cannot recall anything from his former life. But when his memories flood back and he remembers the man that killed both him and his wife, he breaks out of the facility to get revenge, only to discover that there's more to the conspiracy than he thought.",
+                        7.3),
+                new Movie("Bloodshot 2",
+                        LocalDate.of(2020, 02, 20),
+                        "/4U7hpTK0XTQBKT5X60bKmJd05ha.jpg",
+                        "After he and his wife are murdered, marine Ray Garrison is resurrected by a team of scientists. Enhanced with nanotechnology, he becomes a superhuman, biotech killing machine—'Bloodshot'. As Ray first trains with fellow super-soldiers, he cannot recall anything from his former life. But when his memories flood back and he remembers the man that killed both him and his wife, he breaks out of the facility to get revenge, only to discover that there's more to the conspiracy than he thought.",
+                        7.3),
+                new Movie("Bloodshot 3",
+                        LocalDate.of(2020, 02, 20),
+                        "/6P3c80EOm7BodndGBUAJHHsHKrp.jpg",
+                        "After he and his wife are murdered, marine Ray Garrison is resurrected by a team of scientists. Enhanced with nanotechnology, he becomes a superhuman, biotech killing machine—'Bloodshot'. As Ray first trains with fellow super-soldiers, he cannot recall anything from his former life. But when his memories flood back and he remembers the man that killed both him and his wife, he breaks out of the facility to get revenge, only to discover that there's more to the conspiracy than he thought.",
+                        7.3),
+                new Movie("Bloodshot 4",
+                        LocalDate.of(2020, 02, 20),
+                        "/db32LaOibwEliAmSL2jjDF6oDdj.jpg",
+                        "After he and his wife are murdered, marine Ray Garrison is resurrected by a team of scientists. Enhanced with nanotechnology, he becomes a superhuman, biotech killing machine—'Bloodshot'. As Ray first trains with fellow super-soldiers, he cannot recall anything from his former life. But when his memories flood back and he remembers the man that killed both him and his wife, he breaks out of the facility to get revenge, only to discover that there's more to the conspiracy than he thought.",
+                        7.3),
+                new Movie("Bloodshot 5",
+                        LocalDate.of(2020, 02, 20),
+                        "/h6Wi81XNXCjTAcdstiCLRykN3Pa.jpg",
+                        "After he and his wife are murdered, marine Ray Garrison is resurrected by a team of scientists. Enhanced with nanotechnology, he becomes a superhuman, biotech killing machine—'Bloodshot'. As Ray first trains with fellow super-soldiers, he cannot recall anything from his former life. But when his memories flood back and he remembers the man that killed both him and his wife, he breaks out of the facility to get revenge, only to discover that there's more to the conspiracy than he thought.",
+                        7.3),
+                new Movie("Bloodshot 6",
+                        LocalDate.of(2020, 02, 20),
+                        "/8WUVHemHFH2ZIP6NWkwlHWsyrEL.jpg",
+                        "After he and his wife are murdered, marine Ray Garrison is resurrected by a team of scientists. Enhanced with nanotechnology, he becomes a superhuman, biotech killing machine—'Bloodshot'. As Ray first trains with fellow super-soldiers, he cannot recall anything from his former life. But when his memories flood back and he remembers the man that killed both him and his wife, he breaks out of the facility to get revenge, only to discover that there's more to the conspiracy than he thought.",
+                        7.3));
+    }
+}
---
+++ app/src/main/java/com/example/popularmovies/ui/main/MoviesAdapter.java
@@ -0,0 +1,73 @@
+package com.example.popularmovies.ui.main;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.popularmovies.R;
+import com.example.popularmovies.data.Movie;
+import com.squareup.picasso.Picasso;
+
+import java.util.Collections;
+import java.util.List;
+
+public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.ViewHolder> {
+    private List<Movie> movieList = Collections.emptyList();
+    private OnItemClickListener listener;
+
+    public MoviesAdapter() {
+    }
+
+    @NonNull
+    @Override
+    public MoviesAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.movie_item_layout, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MoviesAdapter.ViewHolder holder, int position) {
+        holder.bind(movieList.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return movieList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        private View itemView;
+        private ImageView moviePoster;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            this.itemView = itemView;
+            moviePoster = itemView.findViewById(R.id.movie_iv);
+        }
+
+        public void bind(Movie movie) {
+            Picasso.get()
+                    .load(movie.getPosterPath())
+                    .placeholder(R.drawable.placeholder)
+                    .into(moviePoster);
+            itemView.setOnClickListener(v -> listener.onItemClick(movie));
+        }
+    }
+
+    public void setListener(OnItemClickListener listener) {
+        this.listener = listener;
+    }
+
+    public void setMovieList(List<Movie> movieList) {
+        this.movieList = movieList;
+        this.notifyDataSetChanged();
+    }
+
+    public interface OnItemClickListener {
+        void onItemClick(Movie movie);
+    }
+}
